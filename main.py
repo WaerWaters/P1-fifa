8719b0f6ae9766128421df87b8a2e1ec4dc1eac9
WaerWaters <daniel.johnsen1@hotmail.com>
2022-10-06 12:32:46 +0200

    deleted spaces in variables with two words

--- main.py
+++ main.py
@@ -5,20 +5,31 @@ df = pd.read_csv('data.csv', encoding='UTF-8')
 df=df.replace('\ufffd','aaa',regex=True)
 
 #Finder navne med specielle karakterer og fremviser dem
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    count = 0
-    names = []
-    for x in df.Name:
-        if "aaa" in x:
-            names.append(x)
-            count += 1   
-    print(count)
-    print(names)
+def names_with_special_characters():
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        count = 0
+        names = []
+        for x in df.Name:
+            if "aaa" in x:
+                names.append(x)
+                count += 1   
+        print(count)
+        print(names)
 
-#Tjekker hvorvidt alle værdiger under "value" er den samme valuta
+
+#Tjekker hvorvidt alle værdiger under "Value" er den samme valuta
+def check_valuta():
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        count = 0
+        for x in df.Value:
+            if "€" in x:
+                count += 1   
+        print(count)
+
+#Tjekker data typerne på hver colonne
+def check_datatypes():
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df.dtypes)
+
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    count = 0
-    for x in df.Value:
-        if "€" in x:
-            count += 1   
-    print(count)
+    print(df)
