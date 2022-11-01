c15ddd019232ee4ac54f2c77c00b0644dfb3f1ce
WaerWaters <daniel.johnsen1@hotmail.com>
2022-11-01 12:06:11 +0100

    working version 3.0

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ import pprint
 
 # Læser datasættet og erstatter specielle tegn som den ikke var istand til at indkode med orden "aaa"
 df = pd.read_csv('data.csv', encoding='UTF-8')
-df = df.replace('\ufffd', 'WZQ', regex=True)
-df = df.replace('\u015f', 'WZQ', regex=True)
-df = df.replace('\u011f', 'WZQ', regex=True)
-df = df.replace('\u0130', 'WZQ', regex=True)
-df = df.replace('\u0148', 'WZQ', regex=True)
-df = df.replace('\u0144', 'WZQ', regex=True)
-df = df.replace('\u0119', 'WZQ', regex=True)
-df = df.replace('\u0142', 'WZQ', regex=True)
-df = df.replace('\u015a', 'WZQ', regex=True)
-df = df.replace('\u0105', 'WZQ', regex=True)
-df = df.replace('\u0219', 'WZQ', regex=True)
-df = df.replace('\u021b', 'WZQ', regex=True)
-df = df.replace('\u017a', 'WZQ', regex=True)
-df = df.replace('\u0141', 'WZQ', regex=True)
+df = df.replace('\ufffd','WZQ', regex=True)
+df = df.replace('\u011f','WZQ', regex=True)
+df = df.replace('\u0130','WZQ', regex=True)
+df = df.replace('\u0148','WZQ', regex=True)
+df = df.replace('\u0144','WZQ', regex=True)
+df = df.replace('\u0119','WZQ', regex=True)
+df = df.replace('\u015f','WZQ', regex=True)
+df = df.replace('\u0142','WZQ', regex=True)
+df = df.replace('\u015a','WZQ', regex=True)
+df = df.replace('\u0105','WZQ', regex=True)
+df = df.replace('\u0219','WZQ', regex=True)
+df = df.replace('\u021b','WZQ', regex=True)
+df = df.replace('\u017a','WZQ', regex=True)
+df = df.replace('\u0141','WZQ', regex=True)
 df = df.replace('\u00FC','WZQ', regex=True)
 df = df.replace('\u00E9','WZQ', regex=True)
 df = df.replace('\u00F3','WZQ', regex=True)
@@ -48,7 +48,7 @@ df = df.replace('\u00D6','WZQ', regex=True)
 df = df.replace('\u00E4','WZQ', regex=True)
 df = df.replace('\u00DF','WZQ', regex=True)
 df = df.replace('\u00C1','WZQ', regex=True)
-df = df.replace('\xa0', ' ', regex=True)
+df = df.replace('\xa0',' ', regex=True)
 
 def convert(money):
     df[money] = df[money].str.replace("€", "")
@@ -76,17 +76,20 @@ convert("Value")
 drop_list = ["Name", "Special", "PreferredFoot", "InternationalReputation", "WeakFoot", "SkillMoves", "WorkRate", "BodyType", "RealFace", "Position", "JerseyNumber", "Joined", "LoanedFrom",
              "ContractValidUntil", "Height", "Weight", "Age", "Photo", "Nationality", "Flag", "Overall", "Potential", "Club", "ClubLogo", "BestPosition", "BestOverallRating", "ReleaseClause"]
 attributes = df.drop(columns=drop_list)
-positions = {
+positions = { #changes made here must also be made in the function; splitting_key in position_list
         "GK": [167495, 235073, 190941],
         "RB": [226851, 202371, 253149],
         "LB": [234396, 209889, 239368],
-        "CB": [212190, 229237, 247263, 220814, 178603, 213331],
-        "CDM": [209658, 218339, 212242, 212622, 252371, 211748],
+        "CB1": [212190, 229237, 247263],
+        "CB2": [220814, 178603, 213331],
+        "CDM1": [209658, 218339, 212242],
+        "CDM2": [212622, 252371, 211748],
         "COM": [189596, 188350, 213955],
         "HM": [206113, 212194, 202857],
         "LM": [213345, 203486, 241852],
         "ST": [188545, 239085, 234236],
     }
+icons = [5003, 7826, 3647, 250, 388, 1183, 48940, 34079, 7512, 53769, 1075, 31432, 45674, 9676, 7289, 13743, 241, 1625, 1198, 138449, 330, 11141, 5680, 121939, 5471, 5589, 1668, 1109, 1088, 5419, 7763, 45661, 23174, 4231, 1040, 28130, 37576, 246, 1256, 13128, 49369, 5984, 51539, 10264, 140601, 10535, 5099, 1041]
 
 
 def ideal_attributes():
@@ -113,11 +116,9 @@ def ideal_attributes():
             ideal_player_attributes[attribute] = attribute_average
         ideal_player_stats[position] = ideal_player_attributes.copy()
         ideal_player_attributes.clear()
-    ideal_player_stats_json = json.dumps(ideal_player_stats, indent=2)
-    #print(ideal_player_stats_json)
     return ideal_player_stats
 
-#ideal_attributes()
+#pprint.pprint(ideal_attributes())
 
 def top8(ideal_attributes):
     ideal_attributes = ideal_attributes
@@ -126,8 +127,6 @@ def top8(ideal_attributes):
         attributes = ideal_attributes.get(position)
         top8 = sorted(attributes, key=attributes.get, reverse=True)[2:10]
         result[position] = top8
-    result_json = json.dumps(result)
-    #print(result_json)
     return result
 
 #pprint.pprint(top8(ideal_attributes()))
@@ -135,27 +134,17 @@ def top8(ideal_attributes):
 
 def splitting_key():
     team_list = ["FC Bayern MWZQnchen", "Borussia Dortmund", "Leverkusen"]
-    position_list = ["GK","RB","LB","CB","CDM","COM", "HM","LM","ST"]
+    position_list = ["GK","RB","LB","CB1","CB2","CDM1","CDM2","COM","HM","LM","ST"]
     # calculate cost of each positions on each team (float)
     ideal_team_cost = {}
     ideal_player_cost = {}
     for x in range(len(team_list)):
         ideal_player_cost.clear()
         for position in positions:
-            if position == "CB" or position == "CDM":
-                first = positions.get(position)[x]
-                second = positions.get(position)[x+3]
-                first = float(df.loc[df["ID"] == first, "Wage"] * 52 + df.loc[df["ID"] == first, "Value"])
-                second = float(df.loc[df["ID"] == second, "Wage"] * 52 + df.loc[df["ID"] == second, "Value"])
-                value = (first + second)/2
-                ideal_player_cost[position] = value
-            else:
-                id = positions.get(position)[x]
-                value = float(df.loc[df["ID"] == id, "Wage"] * 52 + df.loc[df["ID"] == id, "Value"])
-                ideal_player_cost[position] = value
+            id = positions.get(position)[x]
+            value = float(df.loc[df["ID"] == id, "Wage"] * 52 + df.loc[df["ID"] == id, "Value"])
+            ideal_player_cost[position] = value
         ideal_team_cost[team_list[x]] = ideal_player_cost.copy()
-    ideal_team_cost_json = json.dumps(ideal_team_cost, indent=2)
-    #print(ideal_team_cost_json)
 
     # calculate ratio cost of each position on each team (float)
     ideal_player_ratio_cost_club = {}
@@ -171,30 +160,24 @@ def splitting_key():
             ideal_player_ratio_cost_club[position_cost] = position_ratio_cost
         ideal_team_ratio_cost_club[club] = ideal_player_ratio_cost_club.copy(
         )
-    ideal_team_cost_procent_club_json = json.dumps(ideal_team_ratio_cost_club, indent=2)
-    #print(ideal_team_cost_procent_club_json)
 
     # calculate average cost in procent of each position from each team (float)
     ideal_team_cost_ratio = {}
     for position in position_list:
         position_ratio = 0
         for club in team_list:
-            # print(ideal_team_ratio_cost_club[club].get(position))
             position_ratio += ideal_team_ratio_cost_club[club].get(position)
     
         average_ratio_position = position_ratio / len(team_list)
         ideal_team_cost_ratio[position] = average_ratio_position
-        ideal_team_cost_ratio_json = json.dumps(ideal_team_cost_ratio, indent=2)
-    #print(ideal_team_cost_ratio_json)
     ideal_team_cost_ratio_sorted = {}
     ratio_to_position = ideal_team_cost_ratio
     ideal_team_cost_ratio_sorted = sorted(ratio_to_position, key=ratio_to_position.get, reverse=True)
-    #print(ideal_team_cost_ratio_sorted)
     return [ideal_team_cost_ratio, ideal_team_cost_ratio_sorted]
 
-#print(splitting_key()[1])
+#pprint.pprint(splitting_key())
 
-not_eligible_teams = ["FC Bayern MWZQnchen","Borussia Dortmund","Bayer 04 Leverkusen","RB Leipzig","1. FC Union Berlin","Sport-Club Freiburg","1. FC KWZQln","1. FSV Mainz 05","TSG Hoffenheim","Borussia MWZQnchengladbach","Eintracht Frankfurt","VfL Wolfsburg","VfL Bochum 1848","FC Augsburg","Vfb Stuttgart","Hertha BSC","DSC Arminia Bielefeld","SpVgg Greuther FWZQrth"]  # de hold som er i samme liga som holdet.
+not_eligible_teams = ["FC Bayern MWZQnchen","Borussia Dortmund","Bayer 04 Leverkusen","RB Leipzig","1. FC Union Berlin","Sport-Club Freiburg","1. FC KWZQln","1. FSV Mainz 05","TSG Hoffenheim","Borussia MWZQnchengladbach","Eintracht Frankfurt","VfL Wolfsburg","VfL Bochum 1848","FC Augsburg","Vfb Stuttgart","Hertha BSC","DSC Arminia Bielefeld"]  # de hold som er i samme liga som holdet.
     
     
 eligible_players = []
@@ -223,11 +206,12 @@ eligible_players = []
 def player_list_eligible():
     for player in df["ID"]:
         players_club = df.loc[df["ID"] == player, "Club"].values[0]
-        if players_club in not_eligible_teams:
+        if player in icons:
+            continue
+        elif players_club in not_eligible_teams:
             continue
         else:
             eligible_players.append(player)
-
     return eligible_players
 
 
@@ -237,8 +221,10 @@ def find_best_team(splitting_key, player_id_array, ideal_stats_to_pos, top8_valu
     team = {}
     used_player_list = []
     spent = 0
+    residual_cash = 0
     splitting_key[1]
     for position in splitting_key[1]:
+        player_cost = 0
         new_player = {}
         ideal_stats = ideal_stats_to_pos.get(position)
         for player in player_id_array:
@@ -246,7 +232,7 @@ def find_best_team(splitting_key, player_id_array, ideal_stats_to_pos, top8_valu
             sum_difference = 0
             max_price = budget * splitting_key[0].get(position)
             cost = int(df.loc[df["ID"] == player, "Wage"]) * 52 + int(df.loc[df["ID"] == player, "Value"])
-            if cost > max_price or cost == 0:
+            if cost > max_price + residual_cash or cost == 0:
                 continue
             else:
                 attribute_count = 0
@@ -257,7 +243,6 @@ def find_best_team(splitting_key, player_id_array, ideal_stats_to_pos, top8_valu
                         continue
                     if math.isnan(df.loc[df["ID"] == player, attribute]):
                         player_has_Nan = True
-                        #print(player, attribute, player_has_Nan)
                         continue
                     else:
                         attribute_count += 1
@@ -271,20 +256,21 @@ def find_best_team(splitting_key, player_id_array, ideal_stats_to_pos, top8_valu
                     new_potential_player = {}
                     if new_player.get(position) == None:
                         new_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
+                        player_cost = cost
                     else:
                         new_potential_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
                         player_stat_values = new_potential_player[position][2]
                         if player_stat_values < new_player[position][2]:
                             new_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
+                            player_cost = cost
             else: 
-                continue    
+                continue  
+        residual_cash = max_price + residual_cash - player_cost
         used_player_list.append(new_player[position][0])
         spent += new_player[position][3]                
         team[position] = new_player[position].copy()
-        print(new_player)
+        print(new_player, residual_cash)
     procent_spent = spent*100 / budget
-    team_json = json.dumps(team, indent=2)
-    #print(team_json)
     return team, spent, procent_spent
 
 
