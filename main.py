3e81536308a322d1ca38c988f06242a74d5afeb7
WaerWaters <daniel.johnsen1@hotmail.com>
2022-11-18 09:03:21 +0100

    updated version, with better naming

--- main.py
+++ main.py
@@ -38,123 +38,96 @@ positions = { #changes made here must also be made in the function; splitting_ke
         "LM": [213345, 203486, 241852],
         "ST": [188545, 239085, 234236],
     }
-icons = [5003, 7826, 3647, 250, 388, 1183, 48940, 34079, 7512, 53769, 1075, 31432, 45674, 9676, 7289, 13743, 241, 1625, 1198, 138449, 330, 11141, 5680, 121939, 5471, 5589, 1668, 1109, 1088, 5419, 7763, 45661, 23174, 4231, 1040, 28130, 37576, 246, 1256, 13128, 49369, 5984, 51539, 10264, 140601, 10535, 5099, 1041]
+
 
 
 def ideal_attributes():
-    ideal_player_attributes = {}
-    ideal_player_stats = {}
+    ideal_position_attribute = {}
+    ideal_team_attributes = {}
 
     for position in positions:
+        ideal_position_attribute.clear()
         players = positions.get(position)
         for attribute in attributes:
             if attribute == "ID":
                 continue
-            player_nan_count = 0
+            players_with_NaN = 0
             total_attribute_value = 0
             for player in players:
                 if math.isnan(df.loc[df["ID"] == player, attribute]):
-                    player_nan_count += 1
+                    players_with_NaN += 1
                     continue
-                value = int(df.loc[df["ID"] == player, attribute])
-                total_attribute_value += value
-            if len(players) == player_nan_count:
-                ideal_player_attributes[attribute] = float("nan")
+                attribute_value = int(df.loc[df["ID"] == player, attribute])
+                total_attribute_value += attribute_value
+            if len(players) == players_with_NaN:
+                ideal_position_attribute[attribute] = float("nan")
             else:
-                attribute_average = round(total_attribute_value/(len(players) - player_nan_count), 1)
-            ideal_player_attributes[attribute] = attribute_average
-        ideal_player_stats[position] = ideal_player_attributes.copy()
-        ideal_player_attributes.clear()
-    return ideal_player_stats
+                attribute_average = round(total_attribute_value/(len(players) - players_with_NaN), 1)
+            ideal_position_attribute[attribute] = attribute_average
+        ideal_team_attributes[position] = ideal_position_attribute.copy()
+    return ideal_team_attributes
 
 #pprint.pprint(ideal_attributes())
 
-def top8(ideal_attributes):
-    ideal_attributes = ideal_attributes
+def top8(ideal_team_attributes):
     result = {}
-    for position in ideal_attributes:
-        attributes = ideal_attributes.get(position)
+    for position in ideal_team_attributes:
+        attributes = ideal_team_attributes.get(position)
         top8 = sorted(attributes, key=attributes.get, reverse=True)[2:10]
         result[position] = top8
     return result
 
-pprint.pprint(top8(ideal_attributes()))
+#pprint.pprint(top8(ideal_attributes()))
 
 
 def splitting_key():
-    team_list = ["FC Bayern MWZQnchen", "Borussia Dortmund", "Leverkusen"]
+    top_clubs = ["FC Bayern MWZQnchen", "Borussia Dortmund", "Leverkusen"]
     # calculate cost of each positions on each team (float)
-    ideal_team_cost = {}
+    top_clubs_costs = {}
     ideal_player_cost = {}
-    for x in range(len(team_list)):
+    for team_id, team in enumerate(top_clubs):
         ideal_player_cost.clear()
         for position in positions:
-            id = positions.get(position)[x]
-            value = float(df.loc[df["ID"] == id, "Wage"] * 52 + df.loc[df["ID"] == id, "Value"])
-            ideal_player_cost[position] = value
-        ideal_team_cost[team_list[x]] = ideal_player_cost.copy()
+            id = positions.get(position)[team_id]
+            total_player_cost = float(df.loc[df["ID"] == id, "Wage"] * 52 + df.loc[df["ID"] == id, "Value"])
+            ideal_player_cost[position] = total_player_cost
+        top_clubs_costs[team] = ideal_player_cost.copy()
 
     # calculate ratio cost of each position on each team (float)
-    ideal_player_ratio_cost_club = {}
-    ideal_team_ratio_cost_club = {}
-    for club in ideal_team_cost:
-        team_cost = 0
-        position_costs_array = ideal_team_cost.get(club)
-        for position_cost in position_costs_array:
-            team_cost += position_costs_array.get(position_cost)
-        for position_cost in position_costs_array:
-            current_position_cost = position_costs_array.get(position_cost)
-            position_ratio_cost = current_position_cost / team_cost
-            ideal_player_ratio_cost_club[position_cost] = position_ratio_cost
-        ideal_team_ratio_cost_club[club] = ideal_player_ratio_cost_club.copy(
-        )
+    club_position_ratio = {}
+    top_clubs_ratios = {}
+    for club in top_clubs_costs:
+        team_total_cost = 0
+        positions_costs = top_clubs_costs.get(club)
+        team_total_cost = sum(positions_costs.values())
+        for position_cost in positions_costs:
+            position_cost_ratio = positions_costs.get(position_cost) / team_total_cost
+            club_position_ratio[position_cost] = position_cost_ratio
+        top_clubs_ratios[club] = club_position_ratio.copy()
 
-    # calculate average cost in procent of each position from each team (float)
-    ideal_team_cost_ratio = {}
+    # calculate average cost ratio of each position from each club (float)
+    ideal_club_cost_ratio = {}
+    ideal_club_ratio_sorted = {}
     for position in positions:
-        position_ratio = 0
-        for club in team_list:
-            position_ratio += ideal_team_ratio_cost_club[club].get(position)
-    
-        average_ratio_position = position_ratio / len(team_list)
-        ideal_team_cost_ratio[position] = average_ratio_position
-    ideal_team_cost_ratio_sorted = {}
-    ratio_to_position = ideal_team_cost_ratio
-    ideal_team_cost_ratio_sorted = sorted(ratio_to_position, key=ratio_to_position.get, reverse=True)
-    return [ideal_team_cost_ratio, ideal_team_cost_ratio_sorted]
+        sum_position_ratio = 0
+        for club in top_clubs:
+            sum_position_ratio += top_clubs_ratios[club].get(position)
+        position_average_ratio = sum_position_ratio / len(top_clubs)
+        ideal_club_cost_ratio[position] = position_average_ratio
+    ideal_club_ratio_sorted = sorted(ideal_club_cost_ratio, key=ideal_club_cost_ratio.get, reverse=True)
+    return [ideal_club_cost_ratio, ideal_club_ratio_sorted]
 
 #pprint.pprint(splitting_key())
 
+
 not_eligible_teams = ["FC Bayern MWZQnchen","Borussia Dortmund","Bayer 04 Leverkusen","RB Leipzig","1. FC Union Berlin","Sport-Club Freiburg","1. FC KWZQln","1. FSV Mainz 05","TSG Hoffenheim","Borussia MWZQnchengladbach","Eintracht Frankfurt","VfL Wolfsburg","VfL Bochum 1848","FC Augsburg","Vfb Stuttgart","Hertha BSC","DSC Arminia Bielefeld"]  # de hold som er i samme liga som holdet.
-    
-    
 eligible_players = []
+icon_ids = [5003, 7826, 3647, 250, 388, 1183, 48940, 34079, 7512, 53769, 1075, 31432, 45674, 9676, 7289, 13743, 241, 1625, 1198, 138449, 330, 11141, 5680, 121939, 5471, 5589, 1668, 1109, 1088, 5419, 7763, 45661, 23174, 4231, 1040, 28130, 37576, 246, 1256, 13128, 49369, 5984, 51539, 10264, 140601, 10535, 5099, 1041]
 
-# Nikolai Kaaberbøl
-# Liste med hold:
-# 1.	FC Bayern München
-# 2.	Borussia Dortmund
-# 3.	Bayer 04 Leverkusen
-# 4.	RB Leipzig
-# 5.	1. FC Union Berlin
-# 6.	Sport-Club Freiburg
-# 7.	1. FC Köln
-# 8.	1. FSV Mainz 05
-# 9.	TSG Hoffenheim
-# 10.	Borussia Mönchengladbach
-# 11.	Eintracht Frankfurt
-# 12.	VfL Wolfsburg
-# 13.	VfL Bochum 1848
-# 14.	FC Augsburg
-# 15.	Vfb Stuttgart
-# 16.	Hertha BSC
-# 17.	DSC Arminia Bielefeld
-# 18.	SpVgg Greuther Fürth
-
-def player_list_eligible():
+def eligible_players_list():
     for player in df["ID"]:
         players_club = df.loc[df["ID"] == player, "Club"].values[0]
-        if player in icons:
+        if player in icon_ids:
             continue
         elif players_club in not_eligible_teams:
             continue
@@ -162,64 +135,59 @@ def player_list_eligible():
             eligible_players.append(player)
     return eligible_players
 
+#pprint.pprint(eligible_players_list())
 
 
-def find_best_team(splitting_key, player_id_array, ideal_stats_to_pos, top8_values):
+def find_best_team(splitting_key, players, ideal_stats_to_pos, top8_values):
     budget = int(input("budget: "))
     team = {}
     used_player_list = []
     spent = 0
     residual_cash = 0
-    splitting_key[1]
     for position in splitting_key[1]:
-        player_cost = 0
-        new_player = {}
+        player_chosen = []
         ideal_stats = ideal_stats_to_pos.get(position)
-        for player in player_id_array:
-            player_has_Nan = False
+        for player in players:
+            player_has_NaN = False
             sum_difference = 0
             max_price = budget * splitting_key[0].get(position)
-            cost = int(df.loc[df["ID"] == player, "Wage"]) * 52 + int(df.loc[df["ID"] == player, "Value"])
-            if cost > max_price + residual_cash or cost == 0:
+            player_cost = int(df.loc[df["ID"] == player, "Wage"]) * 52 + int(df.loc[df["ID"] == player, "Value"])
+            if player_cost > max_price + residual_cash or player_cost == 0:
                 continue
             else:
                 attribute_count = 0
                 for attribute in top8_values[position]:
-                    if player_has_Nan:
+                    if player_has_NaN:
                         continue
                     if attribute in ["ID", "Value", "Wage"]:
                         continue
                     if math.isnan(df.loc[df["ID"] == player, attribute]):
-                        player_has_Nan = True
+                        player_has_NaN = True
                         continue
                     else:
                         attribute_count += 1
                         player_attribute_value = float(df.loc[df["ID"] == player, attribute])
-                        ideal_attributes_value = ideal_stats.get(attribute)
-                        difference = abs(player_attribute_value - ideal_attributes_value) * 1  # WEIGHTED NUMBER
-                    sum_difference += difference
-            if player_has_Nan == False:
-                sum_difference_average = sum_difference / attribute_count
+                        ideal_attribute_value = ideal_stats.get(attribute)
+                        difference = abs(player_attribute_value - ideal_attribute_value) * 1  # WEIGHTED NUMBER
+                        sum_difference += difference
+            if player_has_NaN == False:
                 if player not in used_player_list:
-                    new_potential_player = {}
-                    if new_player.get(position) == None:
-                        new_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
-                        player_cost = cost
+                    sum_difference_average = sum_difference / attribute_count
+                    potential_player = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, player_cost]
+                    if player_chosen == []:
+                        player_chosen = potential_player
                     else:
-                        new_potential_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
-                        player_stat_values = new_potential_player[position][2]
-                        if player_stat_values < new_player[position][2]:
-                            new_player[position] = [player, str(df.loc[df["ID"] == player, "Name"].values[0]), sum_difference_average, cost]
-                            player_cost = cost
+                        if potential_player[2] < player_chosen[2]:
+                            player_chosen = potential_player
             else:
                 continue  
-        residual_cash = max_price + residual_cash - player_cost
-        used_player_list.append(new_player[position][0])
-        spent += new_player[position][3]                
-        team[position] = new_player[position].copy()
-        print(new_player, residual_cash)
+        residual_cash = max_price + residual_cash - player_chosen[3]
+        used_player_list.append(player_chosen[0])
+        spent += player_chosen[3]                
+        team[position] = player_chosen
+        print(player_chosen, residual_cash)
     procent_spent = spent*100 / budget
     return team, spent, procent_spent
 
 
-#pprint.pprint(find_best_team(splitting_key = splitting_key(), player_id_array= player_list_eligible(), ideal_stats_to_pos= ideal_attributes(), top8_values = top8(ideal_attributes())))
+pprint.pprint(find_best_team(splitting_key = splitting_key(), players= eligible_players_list(), ideal_stats_to_pos= ideal_attributes(), top8_values = top8(ideal_attributes())))
